c787ff9841e4c8dbf7a5da1ac306df6eebafdfb4
Seiichi Yonezawa <nzwsch@gmail.com>
2021-09-20 02:45:22 +0900

    Add initial choices

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+"use strict";
+
 const util = require("util");
 const execFile = util.promisify(require("child_process").execFile);
 const { spawn } = require("child_process");
-const { prompt } = require("enquirer");
+const { prompt, Toggle } = require("enquirer");
 
 (async () => {
   const response = await prompt([
@@ -14,60 +16,70 @@ const { prompt } = require("enquirer");
       type: "select",
       name: "database",
       message: "Select database",
-      choices: ["postgresql", "mysql", "sqlite3", "oracle", "sqlserver", "jdbcmysql", "jdbcsqlite3", "jdbcpostgresql", "jdbc"],
+      choices: [
+        { name: "--database=postgresql", message: "postgresql" },
+        { name: "--database=mysql", message: "mysql" },
+        { name: "--database=sqlite3", message: "sqlite3" },
+        { name: "--database=oracle", message: "oracle" },
+        { name: "--database=sqlserver", message: "sqlserver" },
+        { name: "--database=jdbcmysql", message: "jdbcmysql" },
+        { name: "--database=jdbcsqlite3", message: "jdbcsqlite3" },
+        { name: "--database=jdbcpostgresql", message: "jdbcpostgresql" },
+        { name: "--database=jdbc", message: "jdbc" },
+      ],
     },
     {
       type: "multiselect",
       name: "skips",
       message: "Select skips",
+      initial: [0, 1, 2, 4, 5, 6, 8, 11, 12, 14, 15],
       choices: [
-        "--skip-action-mailer",
-        "--skip-action-mailbox",
-        "--skip-action-text",
-        "--skip-active-record",
-        "--skip-active-job",
-        "--skip-active-storage",
-        "--skip-action-cable",
-        "--skip-sprockets",
-        "--skip-spring",
-        "--skip-listen",
-        "--skip-javascript",
-        "--skip-turbolinks",
-        "--skip-jbuilder",
-        "--skip-test",
-        "--skip-system-test",
-        "--skip-bootsnap",
+        { name: "--skip-action-mailer", message: "action-mailer" },
+        { name: "--skip-action-mailbox", message: "action-mailbox" },
+        { name: "--skip-action-text", message: "action-text" },
+        { name: "--skip-active-record", message: "active-record" },
+        { name: "--skip-active-job", message: "active-job" },
+        { name: "--skip-active-storage", message: "active-storage" },
+        { name: "--skip-action-cable", message: "action-cable" },
+        { name: "--skip-sprockets", message: "sprockets" },
+        { name: "--skip-spring", message: "spring" },
+        { name: "--skip-listen", message: "listen" },
+        { name: "--skip-javascript", message: "javascript" },
+        { name: "--skip-turbolinks", message: "turbolinks" },
+        { name: "--skip-jbuilder", message: "jbuilder" },
+        { name: "--skip-test", message: "test" },
+        { name: "--skip-system-test", message: "system-test" },
+        { name: "--skip-bootsnap", message: "bootsnap" },
       ],
     },
     {
       type: "select",
       name: "webpack",
       message: "Select webpack",
-      choices: ["skip-webpack", "webpacker", "react", "vue", "angular", "elm", "stimulus"],
+      choices: [
+        { name: "--skip-webpack-install", message: "skip webpack install" },
+        { name: "--webpacker", message: "webpacker" },
+        { name: "--webpack=react", message: "react" },
+        { name: "--webpack=vue", message: "vue" },
+        { name: "--webpack=angular", message: "angular" },
+        { name: "--webpack=elm", message: "elm" },
+        { name: "--webpack=stimulus", message: "stimulus" },
+      ],
     },
   ]);
-  const APP_PATH = response.APP_PATH;
-  const database = `--database=${response.database}`;
-  const skips = response.skips;
-  let webpack;
-  switch (response.webpack) {
-    case "skip-webpack":
-      webpack = "--skip-webpack-install";
-      break;
-    case "webpacker":
-      webpack = "--webpacker";
-      break;
-    default:
-      webpack = `--webpack=${response.webpack}`;
+  const { APP_PATH, database, skips, webpack } = response;
+  if (APP_PATH === "") {
+    console.error("APP_PATH is empty.");
+    process.exit(0);
   }
   const { stdout } = await execFile("echo", ["rails", "new", APP_PATH, database, ...skips, webpack]);
   process.stdout.write(stdout);
-  const toggle = await prompt({
+  const toggle = await new Toggle({
     type: "toggle",
     message: "Want to run?",
     enabled: "Yep",
     disabled: "Nope",
-  });
+  }).run();
   if (toggle) {
     const rails = spawn("rails", ["new", APP_PATH, database, ...skips, webpack]);
     rails.stdout.on("data", (data) => process.stdout.write(data.toString()));
